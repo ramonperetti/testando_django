f44ce8378a0b2c6b8e64631556eab3a9e796861c
Ramon Peretti <ramonpetti2015@gmail.com>
2021-07-27 22:30:00 -0300

    Primeiro commit

---
+++ app_core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Produto(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    preco = models.DecimalField('Preço', decimal_places=2, max_digits=9)
+    estoque = models.IntegerField('Quantidade em estoque')
+
+class Cliente(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    sobrenome = models.CharField('Sobrenome', max_length=100)
+    email = models.EmailField('Email', max_length=100)
+    
---
+++ app_core/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def index(request):
+    context = {
+        'curso': 'Programação web com python Django',
+        'outro': 'Django é massa'
+    }
+    return render(request, 'index.html', context)
+
+def contato(request):
+    return render(request, 'contato.html')
