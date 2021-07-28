2275a232fc01358ac23cc516481f2c19b47731d7
Ramon Soares Peretti <ramon.peretti@a1.ind.br>
2021-07-28 11:11:34 -0300

    somente manda em dia

--- app_core/views.py
+++ app_core/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from app_core.models import Produto
 
 def index(request):
+    produtos = Produto.objects.all()
+
     context = {
         'curso': 'Programação web com python Django',
-        'outro': 'Django é massa'
+        'outro': 'Django é massa',
+        'produtos': produtos
     }
     return render(request, 'index.html', context)
 
 def contato(request):
     return render(request, 'contato.html')
+
+def produto(request, pk):
+    prod = Produto.objects.get(id=pk)
+
+    context = {
+        'produto': prod
+    }
+    return render(request, 'produto.html', context)
